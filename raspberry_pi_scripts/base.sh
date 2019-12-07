437c4f8b0173b9d6a3af0865ff9f114c165cb413
Ivan Gayton <ivangayton@gmail.com>
2019-12-07 12:56:34 +0300

    Corrected flag in stat command for file size check

--- raspberry_pi_scripts/base.sh
+++ raspberry_pi_scripts/base.sh
@@ -5,19 +5,8 @@
 # (UART) to a uBlox ZED-F9P GNSS receiver.
 #
 # Checks for the presense of serial devices /dev/ttyS0 and /dev/ttyACM0.
-# If it finds them, checks for incoming data. If it finds incoming data
-# launches the str2str utility from RTKLIB to create a .ubx-format GNSS log.
-# Loops 10 times to check for the presence of serial devices and incoming data.
-# No verification of baud rate, or if the data is garbage, or anything other
-# than the presence of incoming bits from the specified devices.
-#
-# Sleeps 30 seconds between tries.
-
-# Make sure everything else on the PI is started before we begin
-sleep 60
-
-# Put a blank line in the log so it's easy to see where restart happened
-echo ""
+# If it finds them, launches str2str utility from RTKLIB to create .ubx log.
+# Checks if that log is growing. If yes, quits. If no, retries several times.
 
 # Make it easier to write to the log with date/time prepended.
 log () { 
@@ -41,7 +30,7 @@ do
     for dev in "fakedevice" "ttyACM0" "ttyS0" #TODO remove fakedevice
     do
 	device=$dev
-	if [ -c $device ]
+	if [ -c "/dev/$device" ]
 	then
 	    outfile="/home/pi/base_log_$(date "+%Y_%m_%d_%H_%M").ubx"
 	    log "Attempting to launch log from $device"
@@ -54,11 +43,11 @@ do
 	    log "$outfile created, checking to see if it's growing"
 	    oldfilesize=$(stat -c%s "$outfile")
 	    sleep 10
-	    newfilesize=$(stat -s%s "$outfile")
+	    newfilesize=$(stat -c%s "$outfile")
 	    log "Started at $oldfilesize and after 10 seconds $newfilesize"
 	    if [ $newfilesize -gt $oldfilesize ]
 	    then
-		log "$outfile is growing"
+		log "$outfile is growing. Survey in progress."
 		exit 0
 	    else
 		log "Nope, it is not growing, let us try again"
